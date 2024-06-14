87457d09157279138676e725a82c26eec0ac3d51
PREM VISHWAKARMA <147607826+pvishwakarma09@users.noreply.github.com>
2024-06-14 20:37:00 +0530

    cards add

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css';
+import Daily from './components/Daily';
 
 const App = () => {
   return (
+    <>
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/foryou" element={<div>For You Page</div>} />
-        <Route path="/following" element={<div>Following Page</div>} />
-        <Route path="/explore" element={<div>Explore Page</div>} />
-        <Route path="/chat" element={<div>Chat Page</div>} />
-        <Route path="/newpage" element={<div>New Page</div>} />
+        <Route path="/foryou" element={<div></div>} />
+        <Route path="/following" element={<div></div>} />
+        <Route path="/explore" element={<div></div>} />
+        <Route path="/chat" element={<div></div>} />
+        <Route path="/newpage" element={<div></div>} />
       </Routes>
     </Router>
+    <Daily/>
+    </>
   );
 };
 
---
+++ src/components/Daily.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Daily.css";
+
+const Daily = () => {
+  return (
+    <>
+    <h1>Recomonded for you</h1>
+    <div className="main-section">
+      <div className="first-card fisrt">first Video</div>
+      <div className="first-card second">Second Video</div>
+      <div className="first-card third">Third Video</div>
+      <div className="first-card four">fourth Video</div>
+      <div className="first-card fifth">first Video</div>
+      <div className="first-card six ">first Video</div>
+      <div className="first-card seven">first Video</div>
+      <div className="first-card eight">first Video</div>
+      <div className="first-card nine">first Video</div>
+      <div className="first-card ten">Second Video</div>
+      <div className="first-card eleven">Third Video</div>
+      <div className="first-card twelev">fourth Video</div>
+      <div className="first-card thirteen">first Video</div>
+      <div className="first-card fourteen ">first Video</div>
+      <div className="first-card fifthteen">first Video</div>
+      <div className="first-card sixteen">first Video</div>
+      <div className="first-card eighteen ">first Video</div>
+      <div className="first-card nineteen ">first Video</div>
+      <div className="first-card twenty ">first Video</div>
+      <div className="first-card  twentyOne">first Video</div>
+      {/* <div className="first-card twentytwo ">first Video</div> */}
+    </div>
+    </>
+  );
+};
+
+export default Daily;
